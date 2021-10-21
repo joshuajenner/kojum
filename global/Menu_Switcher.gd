e0b7c4ac0dcc399b241263766ac172e676c027ff
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2021-10-22 00:08:04 +0800

    Menu switcher no longer blocks the screen

--- global/Menu_Switcher.gd
+++ global/Menu_Switcher.gd
@@ -3,16 +3,19 @@ extends CanvasLayer
 
 #signal menu_switched()
 
+onready var control = $Control
 onready var anim = $Anim
 onready var fill = $Control/Fill
 
 func _ready():
-	pass # Replace with function body.
+	layer = -1
 
 
 func switch_menu(path):
+	layer = 10
 	anim.play("fade")
 	yield(anim, "animation_finished")
 	assert(get_tree().change_scene(path) == OK)
 	anim.play_backwards("fade")
+	layer = -1
 	
